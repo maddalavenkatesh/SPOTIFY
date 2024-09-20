17a0167c6aea69c58de46badb797d36c3f0f35b0
Venkatesh M <venky9.in@gmail.com>
2024-09-20 17:49:24 +0530

    Updated playlist

--- playlist.tf
+++ playlist.tf
@@ -10,7 +10,9 @@ data "spotify_search_track" "eminem" {
 
 resource "spotify_playlist" "slimshady" {
     name = "Slim Shady"
-  tracks = [data.spotify_search_track.eminem.tracks[0].id,
-  data.spotify_search_track.eminem.tracks[1].id,
-  data.spotify_search_track.eminem.tracks[2].id]
+  tracks = [
+     data.spotify_search_track.eminem.tracks[0].id,
+     data.spotify_search_track.eminem.tracks[1].id,
+     data.spotify_search_track.eminem.tracks[2].id
+  ]
 }
